d2654cc02b77f143c280c9bbe8609b02bf1ece78
James Borocz <thesilentc@me.com>
2018-08-14 12:52:33 -0400

    added notation re: bcrypt gem

--- host-creator/db/schema.rb
+++ host-creator/db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digest"
+    t.string "password_digest" # BCrypt gem
   end
 
 end
