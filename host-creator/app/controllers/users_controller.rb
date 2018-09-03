ca29f5176efdcb1a34372d9b5f7f23bf406d5e13
thesilentc <thesilentc@me.com>
2018-09-03 14:20:11 -0400

    romove post/login notation

--- host-creator/app/controllers/users_controller.rb
+++ host-creator/app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
     end
   end
 
-  post '/login' do #User.find_by(email: params[:email], password: params[:password])
+  post '/login' do 
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
