9fafe1498b936eb288b5eb296ad608c9dc47aac2
James Borocz <thesilentc@me.com>
2018-09-10 19:18:30 -0400

    simplified current_user definition in AppController

--- host-creator/app/controllers/application_controller.rb
+++ host-creator/app/controllers/application_controller.rb
@@ -16,13 +16,25 @@ class ApplicationController < Sinatra::Base
   helpers do
 
     def logged_in?
-      !!current_user
+        !!session[:user_id]
     end
 
-    def current_user  # ':id' => url variable
-      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+      def current_user
+        User.find(session[:user_id]) if session[:user_id]
+      end
     end
-
-  end
 
 end
+
+
+#     def logged_in?
+#       !!current_user
+#     end
+#
+#     def current_user  # ':id' => url variable
+#       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+#     end
+#
+#   end
+#
+# end
