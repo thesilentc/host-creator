a70fe24b208e366768fea0f96a2ab9767bee8d4c
James Borocz <thesilentc@me.com>
2018-10-02 20:41:43 -0400

    db

--- host-creator/db/schema.rb
+++ host-creator/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "hosts", force: :cascade do |t|
     t.text     "content"
